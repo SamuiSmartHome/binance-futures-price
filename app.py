10e87f5bf728e25175bb6ae62e2e8eb6b3692b96
Sunonline <32479379+SamuiSmartHome@users.noreply.github.com>
2023-05-03 11:14:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ A simple cryptocurrency price app pulling price data from *Binance API*.
 st.header('**Selected Price**')
 
 # Load market data from Binance API
-df = pd.read_json('https://fapi.binance.com/fapi/v1/ticker/24hr')
+# df = pd.read_json('https://fapi.binance.com/fapi/v1/ticker/24hr')
+df = pd.read_json('https://fapi.binance.com/fapi/v1/ticker/price')
 
 # Custom function for rounding values
 def round_value(input_value):
