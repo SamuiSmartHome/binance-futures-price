cdbc951359689f3eea1143503ab16fa586176e8d
Sunonline <32479379+SamuiSmartHome@users.noreply.github.com>
2023-05-03 11:26:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import binance-futures-connector as cm_futures_clien
+import binance-futures-connector as cm_futures_client
 
 cm_futures_client = CMFutures()
 
